1b0b4afc00bd8120b1f86a5c213071e1fcf19c35
liuchang0812 <liuchang0812@gmail.com>
2016-11-03 15:27:31 +0800

    update qiniu

--- migrate_tool/services/qiniu.py
+++ migrate_tool/services/qiniu.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
+from logging import getLogger
 from migrate_tool import storage_service
 from qiniu import Auth
+from qiniu import BucketManager
+import requests
+
+logger = getLogger(__name__)
 
 
 class QiniuStorageService(storage_service.StorageService):
@@ -11,20 +15,49 @@ class QiniuStorageService(storage_service.StorageService):
 
         accesskeyid = kwargs['accesskeyid']
         accesskeysecret = kwargs['accesskeysecret']
-        bucket = kwargs['bucket']
+        self._bucket = kwargs['bucket']
         self._auth = Auth(accesskeyid, accesskeysecret)
-
-        self._oss_api = oss2.Bucket(oss2.Auth(accesskeyid, accesskeysecret), endpoint, bucket)
+        self._domain = kwargs['domain_url']
+        self._qiniu_api = BucketManager(self._auth)
 
     def download(self, cos_path, local_path):
-        self._oss_api.get_object_to_file(cos_path, local_path)
+        if cos_path.startswith('/'):
+            cos_path = cos_path[1:]
+
+        base_url = 'http://%s/%s' % (self._domain, cos_path)
+        private_url = q.private_download_url(base_url, expires=3600)
+        logger.debug("private url: " + private_url)
+
+        ret = requests.get(private_url)
+
+        if ret.status_code == 200:
+            raise SystemError("download file from qiniu failed")
+
+        with open(local_path, 'wb') as fd:
+            for chunk in ret.iter_content(1024):
+                fd.write(chunk)
 
     def upload(self, cos_path, local_path):
         raise NotImplementedError
 
     def list(self):
-        for obj in oss2.ObjectIterator(self._oss_api):
-            yield obj.key
+        prefix = None
+        limit = 100
+        delimiter = None
+        marker = None
+
+        eof = False
+
+        while not eof:
+            try:
+                ret, eof, info = self._qiniu_api.list(self._bucket, prefix, marker, limit, delimiter)
+                for i in ret['item']:
+                    yield i['key']
+
+                if eof:
+                    marker = ret['marker']
+            except Exception as e:
+                logger.warn("list exception: " + str(e))
 
     def exists(self, _path):
         raise NotImplementedError
