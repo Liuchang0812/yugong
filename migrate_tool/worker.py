c677d2b19bfbacd7d2b34ae1f91a9f621dd7c58c
channingliu(刘畅) <channingliu@tencent.com>
2017-01-27 21:56:37 +0800

    add double delete

--- migrate_tool/worker.py
+++ migrate_tool/worker.py
@@ -53,46 +53,61 @@ class Worker(object):
 
             localpath = path.join(self._work_dir, task_path)
             try:
-                makedirs(path.dirname(localpath))
-            except OSError as e:
-                # directory is exists
-                logger.debug(str(e))
-            try:
-                self._output_service.download(task, localpath)
-            except Exception as e:
-                logger.exception("download failed")
-                self._fail += 1
-                fail_logger.error(task_path)
-                continue
 
-            try:
-                self._input_service.upload(task_path, localpath)
-            except Exception as e:
-                logger.exception("upload {} failed".format(task_path.encode('utf-8')))
-                self._fail += 1
-                fail_logger.error(task_path)
-                continue
-
-            try:
-                import os
-                if isinstance(localpath, unicode):
-                    localpath = localpath.encode('utf-8')
-
-                os.remove(localpath)
                 try:
-                    os.removedirs(path.dirname(localpath))
-                except OSError:
-                    pass
-            except Exception as e:
-                logger.exception(str(e))
-                continue
-            if isinstance(task_path, unicode):
-                logger.info("inc succ with {}".format(task_path.encode('utf-8')))
-            else:
-                logger.info("inc succ with {}".format(task_path.encode('utf-8')))
+                    makedirs(path.dirname(localpath))
+                except OSError as e:
+                    # directory is exists
+                    logger.debug(str(e))
+                try:
+                    self._output_service.download(task, localpath)
+                except Exception as e:
+                    logger.exception("download failed")
+                    self._fail += 1
+                    fail_logger.error(task_path)
+                    continue
 
-            self._succ += 1
-            self._filter.add(task_path)
+                try:
+                    self._input_service.upload(task_path, localpath)
+                except Exception as e:
+                    logger.exception("upload {} failed".format(task_path.encode('utf-8')))
+                    self._fail += 1
+                    fail_logger.error(task_path)
+                    continue
+
+                try:
+                    import os
+                    if isinstance(localpath, unicode):
+                        localpath = localpath.encode('utf-8')
+
+                    os.remove(localpath)
+                    try:
+                        os.removedirs(path.dirname(localpath))
+                    except OSError:
+                        pass
+                except Exception as e:
+                    logger.exception(str(e))
+                    continue
+                if isinstance(task_path, unicode):
+                    logger.info("inc succ with {}".format(task_path.encode('utf-8')))
+                else:
+                    logger.info("inc succ with {}".format(task_path.encode('utf-8')))
+
+                self._succ += 1
+                self._filter.add(task_path)
+            except Exception:
+                logger.exception("try except for deleting file")
+                
+            finally:
+                    import os
+                    if isinstance(localpath, unicode):
+                        localpath = localpath.encode('utf-8')
+
+                    try:
+                        os.remove(localpath)
+                        os.removedirs(path.dirname(localpath))
+                    except OSError:
+                        pass
 
     def add_task(self, task):
         # blocking
