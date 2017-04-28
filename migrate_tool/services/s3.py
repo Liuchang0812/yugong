fcf25c41b1499d0a7ca09572a8d0a59161ebd243
kitmanzheng <zh_opera@163.com>
2017-04-28 17:58:15 +0800

    support s3 cn-north-1

--- migrate_tool/services/s3.py
+++ migrate_tool/services/s3.py
@@ -24,7 +24,8 @@ class S3StorageService(storage_service.StorageService):
                                    aws_secret_access_key=accesskeysecret, 
                                    host='s3.cn-north-1.amazonaws.com.cn')
         else:
-            _s3_api = S3Connection(aws_access_key_id=accesskeyid, aws_secret_access_key=accesskeysecret)
+            _s3_api = S3Connection(aws_access_key_id=accesskeyid,
+                                   aws_secret_access_key=accesskeysecret)
 
         _s3_api = S3Connection(aws_access_key_id=accesskeyid, aws_secret_access_key=accesskeysecret)
         self._bucket_api = _s3_api.get_bucket(bucket)
