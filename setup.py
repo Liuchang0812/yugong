884bde152d89082539c2785f4f4f02d6dea3d3a3
kitmanzheng <kitmanzheng@gmail.com>
2017-12-18 09:33:32 +0800

    update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def long_description():
 
 setup(
     name='cos_migrate_tool',
-    version='0.2.3',
+    version='0.2.4',
     packages=['migrate_tool', 'migrate_tool.services'],
     url='https://www.qcloud.com/',
     license='MIT',
